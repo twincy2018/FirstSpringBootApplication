b0db2661ef26668a4b4be65da5e182b30c260415
TMalhotra <TMalhotra@Xen-613.TI.ads>
2019-01-08 18:09:32 +0530

    spring4soap producer https://www.concretepage.com/spring-boot/spring-boot-soap-web-service-example

---
+++ SpringSoapProducer/src/main/java/com/concretepage/WebAppInitializer.java
@@ -0,0 +1,25 @@
+package com.concretepage;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+public class WebAppInitializer implements WebApplicationInitializer{
+	public void onStartup(ServletContext servletContext) throws ServletException { 
+		AnnotationConfigWebApplicationContext  ctx = new AnnotationConfigWebApplicationContext ();
+		ctx.register(AppConfig.class);
+		ctx.setServletContext(servletContext);
+
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(ctx);
+		servlet.setTransformSchemaLocations(true);
+		
+		Dynamic dynamic = 	servletContext.addServlet("dispather", servlet);
+		dynamic.addMapping("/soapws/*");
+		dynamic.setLoadOnStartup(1);
+	}
+}
